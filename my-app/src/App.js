b53a26c06d7b60d36e8dce3145e8109cc6662609
Rodrigo <rodrigoabarros3103@gmail.com>
2023-11-01 15:06:47 -0300

    implementando o react parte 1

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Container from "./componentes/container";
+
+function App() {
+  return (
+    <>
+    <div>
+      <p>Administrador</p>
+    </div>
+    <Container></Container>
+    </>
+  );
+}
+
+export default App;
